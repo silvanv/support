4eb2d02f7505574722a27c5e89e4eabf8f3d044f
silvanv <vogelsanger@hotmail.com>
2016-04-05 09:46:32 +0200

    update profiles

--- conf/profiles.clj
+++ conf/profiles.clj
@@ -1,7 +1,9 @@
-{:user {:plugins [[venantius/ultra "0.3.2"]
-                  [lein-kibit "0.0.8"]]
+{:user {:plugins [[venantius/ultra "0.4.0"]
+                  [lein-kibit "0.1.2"]
+                  [jonase/eastwood "0.2.2"]
+                  [lein-cljfmt "0.3.0"]]
         :dependencies [[alembic "0.3.2"]
-                       [cljfmt "0.1.7"]
-                       [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]]
+                       [cljfmt "0.3.0"]]
+        :signing {:gpg-key "3B7845E6"}
         :ultra {:color-scheme :solarized_dark}
         :repl-options {:prompt (fn [ns] (str "\u03BB|"ns">" ))}}}
