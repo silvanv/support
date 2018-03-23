54c802cc9c4f059dcc71794fa7a56f1e2842a19c
silvanv <silvan.vogelsanger@deep-impact.ch>
2018-03-23 10:52:25 +0100

    upgrades

--- conf/profiles.clj
+++ conf/profiles.clj
@@ -1,7 +1,7 @@
-{:user {:plugins [[venantius/ultra "0.4.0"]
-                  [lein-kibit "0.1.2"]
-                  [jonase/eastwood "0.2.2"]
-                  [lein-cljfmt "0.3.0"]]
+{:user {:plugins [[venantius/ultra "0.5.2"]
+                  [lein-kibit "0.1.5"]
+                  [jonase/eastwood "0.2.5"]
+                  [lein-cljfmt "0.5.7"]]
         :dependencies [[alembic "0.3.2"]
                        [cljfmt "0.3.0"]]
         :signing {:gpg-key "3B7845E6"}
