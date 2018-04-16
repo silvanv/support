b30b7abd6a19d521db4caaa2cd03ccebfb2386c0
silvanv <silvan.vogelsanger@deep-impact.ch>
2018-04-16 17:19:52 +0200

    remove the lein plugins which do contradict with the intellij repl

--- conf/profiles.clj
+++ conf/profiles.clj
@@ -1,9 +1,3 @@
-{:user {:plugins [[venantius/ultra "0.5.2"]
-                  [lein-kibit "0.1.5"]
-                  [jonase/eastwood "0.2.5"]
-                  [lein-cljfmt "0.5.7"]]
-        :dependencies [[alembic "0.3.2"]
-                       [cljfmt "0.3.0"]]
-        :signing {:gpg-key "3B7845E6"}
-        :ultra {:color-scheme :solarized_dark}
+{:user {:plugins []
+        :dependencies [[alembic "0.3.2"]]
         :repl-options {:prompt (fn [ns] (str "\u03BB|"ns">" ))}}}
